bfb36d6336a311847a24252f66b5aec70c86a1ae
Antonio <antonio.alves@duque.g12.br>
2025-08-28 21:36:33 -0300

    Aula do dia 28 de agosto - Criação de testes unitários

---
+++ backend/src/domain/entities/User.ts
@@ -0,0 +1,26 @@
+export interface UserProps{
+  id?:string
+  name:string
+  email:string
+  password:string
+  avatarUrl?:string
+  createdAt?:Date
+}
+
+export class User{
+  private props:UserProps
+  constructor(props:UserProps){
+    this.props = {
+      ...props,
+      id:props.id??crypto.randomUUID(),
+      createdAt:props.createdAt??new Date()
+    }
+  }
+
+  get id() {return this.props.id}
+  get name() {return this.props.name}
+  get email() {return this.props.email}
+  get password() {return this.props.password}
+  get avatarUrl() {return this.props.avatarUrl}
+  get createdAt() {return this.props.createdAt}
+}
